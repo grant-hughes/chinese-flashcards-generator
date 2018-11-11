4dc4fa4ac5b0d91415d6e49ef8226b0e6d20857f
grant-hughes <grant.hughes@zoho.com>
2018-11-11 21:01:39 +0000

    completed first version

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  console.log('client-side script running...');
+
+  var pageCount = 0;
+
+  $('#add').click(function() {
+    const htmlInput = $('#html-input');
+    pageCount++;
+    $('#page-count').text('Pages: ' + pageCount);
+    $.ajax({
+      url: 'http://localhost:3000/cards',
+      type: 'post',
+      dataType: 'text',
+      contentType: 'text/plain',
+      data: htmlInput.val(),
+      success: function(data, error) {
+        console.log(data);
+
+      }
+    });
+    htmlInput.val('');
+  });
+
+  $('#generate').click(function() {
+    const fileUrl = 'http://localhost:3000/static/file.txt';
+    $('#link').attr('href', fileUrl);
+    $('#link').text(fileUrl);
+  })
+
+  $('#start-over').click(function() {
+    $('#html-input').val('')
+    pageCount = 0;
+    $('#page-count').text('Pages: ' + pageCount);
+    $.ajax({
+      url: 'http://localhost:3000/cards',
+      type: 'delete',
+      success: function(data, error) {
+        console.log('reset cards');
+      }
+    });
+  });
+});
